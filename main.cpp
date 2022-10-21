9f09bcad13ee309241d58bb94ae16150acac58a2
sakkammadam <sakkammadam@yahoo.com>
2022-10-21 01:20:14 -0700

    SA: Updated main implementation to orchestrate directory reading, mapper and shuffle operations

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@
 #include <map>
 #include <tuple>
 #include <vector>
+#include <filesystem>
 #include "FileProcessor.h"
 #include "Mapper.h"
+#include "Shuffler.h"
 
 int main(int argc, char* argv[]) {
     // Declare the file being processed
     //char* filePathBase = argv[1];
     //std::string filePath(filePathBase);
+
 
     // Create a testing object called testingFoo -- the directory path should be filePath - line #13
     FileProcessor testingFoo(
@@ -33,10 +36,24 @@ int main(int argc, char* argv[]) {
 
     // Create a File Processor object that will write output of Mapper class to disk
     FileProcessor testingMap2Disk("mapper",peek);
-    std::string tempDirectory = testingMap2Disk.writeDirectory();
+    std::string mapDirectory = testingMap2Disk.writeDirectory();
 
     // Please log this - @Hal, @Abraham - TODO
-    std::cout << "Output of Mapper operations have been written to " << tempDirectory << std::endl;
+    std::cout << "Output of Mapper operations have been written to " << mapDirectory << std::endl;
+
+
+
+    Shuffler testingShuffle(mapDirectory);
+    std::vector<std::map<std::string, std::map<std::string,size_t>>> blah;
+    blah = testingShuffle.shuffleOperations();
+
+    // let's provide result of shuffle to FileProcessor to write them to disk
+    FileProcessor testingShuffle2disk("shuffler",blah);
+    std::string shuffleDirectory = testingShuffle2disk.writeDirectory();
+
+    // Please log this - @Hal, @Abraham - TODO
+    std::cout << "Output of shuffle operations have been written to " << shuffleDirectory << std::endl;
+
 
     return 0;
 }
